a2568b2ff57ac32b2ba16a86bc6a7951e6ad5a3d
adamye001_fu <adamye001@gmail.com>
2023-07-24 00:37:11 +1000

    Install Entity Framework and add data seed

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -3,14 +3,10 @@
     public class Owner
     {
         public int Id { get; set; }
-
-        public string Name { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gym { get; set; }
-        // If it is only one object, just use its prototype
         public Country Country { get; set; }
-        // Many to Many relationships
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
     }
 }
-
