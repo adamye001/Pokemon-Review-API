108aa3f3dc647b9d99b1c8b9e016655125a2f6ec
adamye001_fu <adamye001@gmail.com>
2023-07-24 12:59:53 +1000

    Complete Repository Pattern & Dependency Injection

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,22 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository 
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        // ICollection can only be shown not edited
+        public ICollection<Pokemon> GetPokemons() 
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
